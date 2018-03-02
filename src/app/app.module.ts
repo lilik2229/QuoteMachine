efc2c4600ef79799a890bc12d0d440f42bde2951
ShunKOZAWA <crewfanq@gmail.com>
2018-03-03 01:10:31 +0900

    作成した機能をモジュールに読み込み

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-
+import { ApiService } from './api.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuoteComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        QuoteComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [
+        ApiService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
