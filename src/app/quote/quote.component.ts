4eb85be2a1367fb5955445198c7fdd00d9505899
ShunKOZAWA <crewfanq@gmail.com>
2018-03-03 00:44:01 +0900

    quoteコンポーネントでquoteを初期設定する処理を追加

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Quote } from '../quote';
+import { ApiService } from '../api.service';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+    private quote:Quote;
+    
+    constructor(
+        private apiService: ApiService
+    ) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.apiService
+            .get()
+            .subscribe(
+                res=>{
+                    this.quote = res;
+                    console.log(this.quote);
+                }
+            );
+    }
 }
