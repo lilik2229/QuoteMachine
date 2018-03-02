307f60f31c4e083fc4f726507a3e05e16bee7e20
ShunKOZAWA <crewfanq@gmail.com>
2018-03-03 00:29:17 +0900

    api serviceのget処理追加

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+import { Quote } from './quote';
+
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'X-Mashape-Key':''
+    })
+};
 
 @Injectable()
 export class ApiService {
+    private quoteUrl='';
+    
+    constructor(
+        private http: HttpClient
+    ){}
 
-  constructor() { }
-
+    get(){
+        return this.http.get<Quote>(this.quoteUrl,httpOptions);
+    }
 }
+
