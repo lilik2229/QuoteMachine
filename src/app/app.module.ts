e297b2a3d7c6ae42f8ea90c30b54f136258d5d10
ShunKOZAWA <crewfanq@gmail.com>
2018-03-03 23:42:09 +0900

    api-mockを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { ApiService } from './api.service';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
     declarations: [
@@ -13,10 +16,13 @@ import { ApiService } from './api.service';
     ],
     imports: [
         BrowserModule,
-        HttpClientModule
+        HttpClientModule,
+        environment.production ?
+            []:HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{delay: 1500})
     ],
     providers: [
-        ApiService
+        ApiService,
+        InMemoryDataService
     ],
     bootstrap: [AppComponent]
 })
