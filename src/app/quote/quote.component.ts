c5e1dcca3c2c30132d08d489a47120ba4ad6ad87
ShunKOZAWA <crewfanq@gmail.com>
2018-03-05 14:13:40 +0900

    切り替えアニメーションを追加

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit} from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import {
+    trigger,
+    state,
+    style,
+    transition,
+    animate,
+    keyframes,
+    query,
+    stagger
+} from '@angular/animations';
 
 import { Quote } from '../quote';
 import { ApiService } from '../api.service';
 
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+    selector: 'app-quote',
+    templateUrl: './quote.component.html',
+    styleUrls: ['./quote.component.css'],
+    animations: [
+        trigger('toggleState', [
+            state('true' , style({ opacity:1.0 })),
+            state('false', style({ opacity:0.0 })),
+            transition('* => *', animate('1000ms'))
+        ])
+    ]
 })
 
 export class QuoteComponent implements OnInit{
     private quote = new Quote();
+    private autoQuote;
     private randomColor;
     private colorList =[
         'Aqua',
@@ -22,6 +40,8 @@ export class QuoteComponent implements OnInit{
         'Purple',
         'RosyBrown'
     ];
+    private interval=10000;
+    private shouldToggle=false;
     
     constructor(
         private apiService: ApiService
@@ -35,27 +55,27 @@ export class QuoteComponent implements OnInit{
     
     private setColor(){
         const color= this.getRandomColor();
-        console.log(color);
         this.randomColor=color;
         document.body.style.background=color;
     }
-
+    
     private updateQuote(){
-        this.setColor();
+        this.shouldToggle = false;
         this.apiService
             .get()
             .subscribe(
                 res=>{
                     this.quote = res;
-                    console.log(this.quote);
+                    this.setColor();
+                    this.shouldToggle = true;
                 }
             );
     }
     
     ngOnInit() {
         this.updateQuote();
-        Observable
-            .interval(3000)
+        this.autoQuote = Observable
+            .interval(this.interval)
             .subscribe(
                 ()=>{
                     this.updateQuote();
